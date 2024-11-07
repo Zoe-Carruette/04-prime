75035db416a93b83ac7f9ef06fbf726d03376ffa
Zoe-Carruette <zoe.carruette@edu.esiee.fr>
2024-11-07 10:27:13 +0000

    04-primes-Zoé CARRUETTE

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
+""" Main --> Calcul d'un nombre premier"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Vérifie si un nombre est premier.
+    
+    Paramètre:
+    p (int): Le nombre à vérifier.
 
+    Returns:
+    bool: True si p est premier, False sinon.
+    """
     # votre code ici
-
-    pass
+    premier=True
+    if p==1:
+        premier = False
+    for i in range (2,int(sqrt(p))+1):
+        if p%i == 0:
+            premier=False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    La fonction principale qui teste des nombres premier de 1 à 100 et les affiches.
+    """
     # vos appels à la fonction secondaire ici
-
+    isprime(3)
+    isprime(22)
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
